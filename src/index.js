a9e7b31ef3b912e0ee4ae2c5c515f5b5b46508df
Mario <mario.galea84@gmail.com>
2025-06-13 20:37:51 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./Server");
-const mockServer = new Server_1.Server(8080);
-mockServer.start();
